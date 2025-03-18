05ce772bb2fc370073a766c5ab9d997f6494ab88
adilko1 <129127448+adiltutun@users.noreply.github.com>
2025-03-18 10:28:14 +0100

    Incijalni commit

---
+++ app/src/main/java/com/example/miwok/MainActivity.kt
@@ -0,0 +1,13 @@
+package com.example.miwok
+
+import android.app.Activity
+import android.content.Context
+import android.os.Bundle
+import android.util.AttributeSet
+import android.view.View
+
+class MainActivity : Activity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)    }
+}
